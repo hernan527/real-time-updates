3be71bff463062d982111a36fdcbe4522a9e6c9a
hernan527 <39295480+hernan527@users.noreply.github.com>
2022-08-17 22:30:19 -0300

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,71 @@
+import {
+  NgModule, Component, enableProdMode, ChangeDetectionStrategy,
+} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { DxDataGridModule, DxSliderModule } from 'devextreme-angular';
+import DataSource from 'devextreme/data/data_source';
+import ArrayStore from 'devextreme/data/array_store';
+
+import { Product, Order, Service } from './app.service';
+
+
+@Component({
+  selector: 'demo-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [Service],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AppComponent {
+  productsDataSource: DataSource;
+
+  orders: ArrayStore;
+
+  updatesPerSecond = 100;
+
+  constructor(service: Service) {
+    this.productsDataSource = new DataSource({
+      store: service.getProducts(),
+      reshapeOnPush: true,
+    });
+
+    this.orders = service.getOrders();
+
+    setInterval(() => {
+      if (service.getOrderCount() > 500000) {
+        return;
+      }
+
+      for (let i = 0; i < this.updatesPerSecond / 20; i++) {
+        service.addOrder();
+      }
+    }, 50);
+  }
+
+  getDetailGridDataSource(product: Product) {
+    return new DataSource({
+      store: this.orders,
+      reshapeOnPush: true,
+      filter: ['ProductID', '=', product.ProductID],
+    });
+  }
+
+  getAmount(order: Order) {
+    return order.UnitPrice * order.Quantity;
+  }
+}
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    DxDataGridModule,
+    DxSliderModule,
+  ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+})
+export class AppModule { }
+
+platformBrowserDynamic().bootstrapModule(AppModule);
